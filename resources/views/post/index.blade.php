06609df0d8367687eaf214d279f692a776610ae0
sadahira29 <s246116@wakayama-u.ac.jp>
2024-02-20 15:50:59 +0900

    index.bladeからリンク化する関数を呼び出す文を追加

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -19,10 +19,10 @@
         <div class="my-4">
             @if (!empty($posts))
                 <ul>
-                    @foreach ($posts as $post)
+                @foreach ($posts as $post)
                         <li class="mb-6 bg-white border rounded-lg p-4">
                             <h2 class="text-lg font-bold mb-2 border-bottom">{{ $post->title }}</h2>
-                            <p class="text-gray-1000 mt-4">{{ $post->body }}</p>
+                            <p class="text-gray-1000 mt-4">{!! nl2br($post->makeLink($post->body)) !!}</p>
                             <div class="flex justify-between mt-8">
                                 <span>
                                     @if ($post->is_liked())
@@ -41,7 +41,6 @@
                                     {{ $post->user->name }} &emsp; {{ $post->updated_at }}
                                 </p>
                             </div>
-                            
                         </li>
                     @endforeach
                 </ul>
