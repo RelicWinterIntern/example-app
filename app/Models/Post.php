59e07218a0114ae27c88071c229923e5d3943b91
sadahira29 <s246116@wakayama-u.ac.jp>
2024-02-20 15:07:39 +0900

    URLをリンク化するロジックの追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -38,4 +38,21 @@ class Post extends Model
             return false;
         }
     }
+
+    // URLをアンカータグでリンク化する
+    public function makeLink($url) {
+        //URL抽出の正規表現
+        $pattern = '/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:@&=+$,%#]+/';
+    
+        //該当する文字列に処理
+        $comment = preg_replace_callback(
+            $pattern,
+            function ($matches) {
+                return '<a href="'.$matches[0].'">'.$matches[0].'</a>';
+            },
+            htmlspecialchars($comment)
+        );
+    
+        return $comment;
+    }
 }
