31527cd680710a2614ba2c0220b2064383ae2df7
mahhawarn <ba0930fk@ed.ritsumei.ac.jp>
2024-02-20 13:20:33 +0900

    blade いいね数

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,6 +12,11 @@ class Post extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function likes()
+    {
+        return $this->belongsTo(Like::class, 'id', 'id');
+    }
 }
 
 
